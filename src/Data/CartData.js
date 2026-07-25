e35c86ea12f53e62bd3bba8a1c41feb58c64e0a3
Vansh Verma <vanshverma6065@gmail.com>
2026-07-25 07:09:54 +0530

    Build

--- src/Data/CartData.js
+++ src/Data/CartData.js
@@ -3,7 +3,7 @@ export const CartData = [
         id: 1,
         name: "Amritashya Ayurveda Pure Himalayan Shilajit",
         category: "Stress & Vitality Capsules",
-        price: 1499,
+        price: 1299,
         quantity: 1,
         image: 
          "https://www.amritashya.in/_next/image?url=https%3A%2F%2Fgqfeipyaxweijhrlrxqt.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fimages%2Fshilajit%2FResin%2520Amritashya%2520Ayurveda%25201.jpg&w=1920&q=75" ,
