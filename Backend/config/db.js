81afc305a6a941c5da5bcc323b0b75ef0940ac7a
Vansh Verma <vanshverma6065@gmail.com>
2026-07-25 08:55:17 +0530

    Add url

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
-        console.log(` MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("Mongo Error Full:", error);
+  }
 };
 
 export default connectDB;
